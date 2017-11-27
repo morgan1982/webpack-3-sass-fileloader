f6f12922778171e3b866044c3bb072f181a96b92
morgan1982 <alexandrisanas@gmail.com>
2017-11-27 03:31:24 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfing = new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})
+
+const config = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    'react-hot-loader',
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: 'file-loader',
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.css'],
+    },
+    output: {
+        path: path.resolve('dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        historyApiFallback: true
+        // compress: true,
+        // port: 3000
+    }
+
+
+};
+
+module.exports = config;
